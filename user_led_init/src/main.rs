ef8f6bbba87a5b109edaaf5dddb4890cd513fe8a
Tremendous1192 <tremendous.duck@gmail.com>
2023-07-30 18:50:43 +0900

    内蔵LEDの初期化プログラムの追加

---
+++ user_led_init/src/main.rs
@@ -0,0 +1,37 @@
+//! 組込みRustのおまじない
+#![no_std] // 必須アトリビュート
+#![no_main] // 必須アトリビュート
+use panic_halt as _; // 必須クレート
+use wio::prelude::*; // ほぼ必須
+use wio_terminal as wio; // 必須クレート
+
+#[wio_terminal::entry] // 必須アトリビュート
+fn main() -> ! {
+    // 初期化
+    // 必須インスタンス
+    let mut peripherals = wio::pac::Peripherals::take().unwrap();
+    let core = wio::pac::CorePeripherals::take().unwrap();
+    let mut clocks = wio::hal::clock::GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut delay = wio::hal::delay::Delay::new(core.SYST, &mut clocks);
+    let sets = wio::Pins::new(peripherals.PORT).split();
+
+    // 内蔵LED
+    let mut user_led = sets.user_led.into_push_pull_output();
+    user_led.set_low().unwrap();
+    // ここまで 初期化
+
+    // 組込みはloop必須
+    loop {
+        // LED点滅
+        user_led.toggle().ok();
+        delay.delay_ms(200_u16);
+    }
+    // ここまでloop処理
+}
+// ここまでmain関数
